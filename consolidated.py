3d8e55e6ba0b5d270b85ba56941745e43a012d1e
Utkarsh Kaushik <ukaushik@algoscale.com>
2015-07-15 18:52:21 +0530

    command line actions and email updates added

--- consolidated.py
+++ consolidated.py
@@ -2,15 +2,26 @@ import csv, json, re
 from py2neo import Graph, Path
 import json, sys
 from alchemyapi import AlchemyAPI
+from smtplib import SMTP
+import datetime
 
+from_addr = "Utkarsh Kaushik<ukaushik@algoscale.com>"
+to_addr = "Utkarsh Kaushik<ukaushik@algoscale.com>, Neeraj Agarwal<neeraj@algoscale.com>"
 
-def leads():
+start = datetime.datetime.now().strftime( "%d/%m/%Y %H:%M" )
+
+csvFilePath = ''
+
+def leads(csvFilePath):
 	print "Storing Leads"
 	graph = Graph("http://neo4j:root@localhost:7474/db/data/")
 
-	csvfile = open('MarketoLeads.csv')
+	csvfile = open(csvFilePath)
 
 	reader = csv.DictReader(csvfile)
+
+	reslt = {}
+	indi_count = 0
 
 	for row in reader:
 		try:
@@ -32,6 +43,7 @@ def leads():
 				"""
 				individual = graph.cypher.execute(individual_node)
 
+				indi_count = indi_count + 1
 
 				if company!= '':
 					company_node = """ MERGE (company:Company{name:'"""+company+"""'})
@@ -78,17 +90,20 @@ def leads():
 			print e
 			pass
 
-	return "200"
+	reslt['individuals'] = str(indi_count)
 
-def articles_func():
+	return reslt
+
+def articles_func(csvFilePath):
 	
 	print "storing articles"
 	graph = Graph("http://neo4j:root@localhost:7474/db/data/")
 
-	csvfile = open('Web_Page_Activity.csv')
+	csvfile = open(csvFilePath)
 
 	reader = csv.DictReader(csvfile)
 	articles = {}
+	art_count = 0
 	for row in reader:
 
 		try:
@@ -113,7 +128,7 @@ def articles_func():
 					Return article;
 					"""
 					article = graph.cypher.execute(article_node)
-
+					art_count = art_count + 1
 					graph.create(Path(individual.one,"read",article.one))
 
 
@@ -121,7 +136,7 @@ def articles_func():
 			print e
 			pass
 
-	return "200"
+	return {'articles': str(art_count)}
 
 
 def key_entity():
@@ -142,6 +157,9 @@ def key_entity():
 
 	result = graph.cypher.execute(article_query)
 
+	keyword_count = 0
+	entity_count = 0
+	art_count = 0
 	for arti in result:
 
 		if count >= 1000:
@@ -161,6 +179,7 @@ def key_entity():
 			art_keywords[art] = []
 			response = alchemyapi.keywords('url', art, {'sentiment': 1})
 			count = count + 1
+			art_count = art_count + 1
 			if response['status'] == 'OK':
 				for keyword in response['keywords']:
 					# print('text: ', keyword['text'].encode('utf-8'))
@@ -180,6 +199,7 @@ def key_entity():
 					at_keywords = graph.cypher.execute(keyword_node)
 					pth = Path(article.one,("has_keyword",rel_dict),at_keywords.one)
 					graph.create(pth)
+					keyword_count = keyword_count + 1
 
 
 		if count >= 1000:
@@ -212,23 +232,79 @@ def key_entity():
 					pth = Path(article.one,("has_entity",rel_dict),at_entities.one)
 					graph.create(pth)
 					
+					entity_count  = entity_count + 1
 
-	return "200"
+	return {'articles':str(art_count),'keywords':str(keyword_count),'entities':str(entity_count)}
 
-# try:
-# 	first = leads()
-# except Exception,e:
-# 	print e
-# 	pass
 
-# try:
-# 	second = articles_func()
-# except Exception,e:
-# 	print e
-# 	pass
+def emailUpdate(from_addr,to_addr,message):
+	smtp = SMTP()
+	smtp.connect('smtp.mailgun.org', 587)
+	smtp.login('postmaster@nogpo.com', 'f63f5059d3255612cfae4470ed9b34ed')
+	smtp.sendmail(from_addr, to_addr, message)
+	smtp.quit()
 
-try:
-	third = key_entity()
-except Exception,e:
-	print e
-	pass
+
+if len(sys.argv)<=1:
+	print "insufficient parameters"
+	msg = """
+	Hi, \n 
+	This is an update related to the job triggered on """+str(start)+""", for the insertion of data in your Neo4J graph base. \n
+	The code could not complete the run because of insufficient parameters supplied to the query \n 
+	Regards """
+
+	subj = "Update for uLytics job on Neo4J"
+
+
+	msgBod = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % ( from_addr, to_addr, subj, start, msg )
+	
+	emailUpdate(from_addr,to_addr,msgBod)
+	
+	exit()
+else:
+	action = sys.argv[1]	
+
+	if action == 'leads':
+		csvFilePath = sys.argv[2]
+		try:
+			resultDict = leads(csvFilePath)
+		except Exception,e:
+			print e
+			pass
+
+	elif action == 'articles':
+		csvFilePath = sys.argv[2]
+		try:
+			resultDict = articles_func(csvFilePath)
+		except Exception,e:
+			print e
+			pass
+
+	else:
+		try:
+			resultDict = key_entity()
+		except Exception,e:
+			print e
+			pass
+
+end = datetime.datetime.now().strftime( "%d/%m/%Y %H:%M" )
+
+msg = """
+Hi, \n 
+This is an update related to the job triggered on """+str(start)+""", for the insertion of data in your Neo4J graph base. \n
+The Job ended at: """+str(end)+""". \n 
+The code run took place for the action related to: '"""+action+"""' \n 
+It ran for """
+
+for key in resultDict.keys():
+	msg = msg + resultDict[key] + " " + key + ", "
+
+msg = msg.strip(', ')
+msg = msg + " \n\nRegards\n"
+subj = "Update for uLytics job on Neo4J"
+
+
+msgBod = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % ( from_addr, to_addr, subj, end, msg )
+
+
+emailUpdate(from_addr,to_addr,msgBod)
